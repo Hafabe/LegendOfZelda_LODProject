f18f3f168f934be4e48cd40ee04c22061ed303df
Hafabe <Ralph@192.168.1.3>
2013-12-30 16:44:24 -0500

    These files represent the code I created to scrape the data from the ZeldaWiki website. It is far from perfect but it is also my first datascraping program (my first program I have written) and it mostly works, so I am proud of it. There will be updates to the code in order to streamline its output.

---
+++ ZeldaScrapeBosses.py
@@ -0,0 +1,105 @@
+import requests
+import time
+import csv
+from bs4 import BeautifulSoup
+
+f = open('bossesNames.txt','a')
+
+r = requests.get('http://zeldawiki.org/Category:Bosses')
+
+soup = BeautifulSoup(r.text)
+
+nameDiv = soup.find("div", { "id" : "mw-pages" })
+
+nameLi= nameDiv.select('li a')
+
+for links in nameLi:
+
+	f.write((links.text + ',' + links['href'] + '\n').encode('utf-8'))
+	#triple = ''
+	#triple = '<http://zeldawiki.org' + links['href'] + '>'
+# the variable triple is currently not being used.
+#f.close()
+	f2 = open('bossesData.txt', 'a')
+
+	r2 = requests.get('http://zeldawiki.org' + links['href'])
+
+	time.sleep(1)
+
+	print 'requesting:', 'http://zeldawiki.org' + links['href']
+
+	soup2 = BeautifulSoup(r2.text)
+
+	canonCheck = soup2.find('table', {'class' :'gradient'}) 
+	if canonCheck is not None:
+		checkingCanon = canonCheck.select('a')
+		print "Dude, I'm gonna check the canon."
+		if checkingCanon is not None:
+			canonLink = raw_input('Oh schnap son! This Canon for' + ' ' + links.text + ' ' + 'is all suspect brah! Do you want me to toss her?')
+		#True == 'y'
+		#False == 'n'
+			if canonLink == 'y':
+				print "Aight, I'm tossing!"
+				pass
+			elif canonLink == 'n':
+				print "Cool dude, I'm gonna check for a box." 
+				infoBox = soup2.find('table', {'class' : 'toccolours'}) or soup2.find('table', {'class' : 'wikitable'})
+
+				if infoBox is not None:
+
+					print "We have a box!"
+
+					rows = infoBox.select('tr')
+
+					for tableHeader in rows:
+
+						tableHeader.select('th')
+
+						f2.write((tableHeader.text + ',').encode('utf-8'))
+
+					for tableLinks in rows:
+
+						MyTableLinks = tableLinks.find('a')
+
+						if MyTableLinks is not None:
+							#if MyTableLinks 
+							f2.write(('\n' + MyTableLinks['href'] + ',' +'\n' + MyTableLinks.text + ',' +'\n' + tableHeader.text + ',' + '\n').encode('utf-8'))
+
+							print "Grabbing box info brah!!!"
+				else:
+
+					print "No box info brah :( Bummer."
+
+					f2.write('\n' + '\n' + links.text + " has no box info brah. Bummer." + '\n' + '\n')
+	elif canonCheck is None: 
+		infoBox = soup2.find('table', {'class' : 'toccolours'}) or soup2.find('table', {'class' : 'wikitable'})
+
+		if infoBox is not None:
+
+			print "We have a box"
+
+			rows = infoBox.select('tr') 
+
+			for tableHeader in rows:
+
+				tableHeader.select('th')
+
+				f2.write((tableHeader.text + ',').encode('utf-8'))
+
+			for tableLinks in rows:
+
+				MyTableLinks = tableLinks.find('a')
+
+				if MyTableLinks is not None:
+					#if MyTableLinks 
+					f2.write(('\n' + MyTableLinks['href'] + ',' +'\n' + MyTableLinks.text + ',' +'\n' + tableHeader.text + ',' + '\n').encode('utf-8'))
+
+					print "Grabbing box info brah!!!"
+		else:
+
+			print "No box info brah :( Bummer."
+
+			f2.write('\n' + '\n' + links.text + " has no box info brah. Bummer." +'\n' + '\n' )
+
+f.close()
+f2.close()
---
+++ ZeldaScrapeEnemies.py
@@ -0,0 +1,313 @@
+import requests
+import time
+import csv
+from bs4 import BeautifulSoup
+
+f = open('enemyNames.txt','a')
+
+r = requests.get('http://zeldawiki.org/index.php?title=Category:Enemies&pageuntil=Hover#mw-pages')
+
+soup = BeautifulSoup(r.text)
+
+nameDiv = soup.find("div", { "id" : "mw-pages" })
+
+nameLi= nameDiv.select('li a')
+
+for links in nameLi:
+
+	f.write((links.text + ',' + links['href'] + '\n').encode('utf-8'))
+	#triple = ''
+	#triple = '<http://zeldawiki.org' + links['href'] + '>'
+# the variable triple is currently not being used.
+#f.close()
+	f2 = open('enemyData.txt', 'a')
+
+	r2 = requests.get('http://zeldawiki.org' + links['href'])
+
+	time.sleep(1)
+
+	print 'requesting:', 'http://zeldawiki.org' + links['href']
+
+	soup2 = BeautifulSoup(r2.text)
+
+	canonCheck = soup2.find('table', {'class' :'gradient'}) 
+	if canonCheck is not None:
+		checkingCanon = canonCheck.select('a')
+		print "Dude, I'm gonna check the canon."
+		if checkingCanon is not None:
+			canonLink = raw_input('Oh schnap son! This Canon for' + ' ' + links.text + ' ' + 'is all suspect brah! Do you want me to toss her?')
+		#True == 'y'
+		#False == 'n'
+			if canonLink == 'y':
+				print "Aight, I'm tossing!"
+				pass
+			elif canonLink == 'n':
+				print "Cool dude, I'm gonna check for a box." 
+				infoBox = soup2.find('table', {'class' : 'toccolours'}) or soup2.find('table', {'class' : 'wikitable'})
+
+				if infoBox is not None:
+
+					print "We have a box!"
+
+					rows = infoBox.select('tr')
+
+					for tableHeader in rows:
+
+						tableHeader.select('th')
+
+						f2.write((tableHeader.text + ',').encode('utf-8'))
+
+					for tableLinks in rows:
+
+						MyTableLinks = tableLinks.find('a')
+
+						if MyTableLinks is not None:
+							#if MyTableLinks 
+							f2.write(('\n' + MyTableLinks['href'] + ',' + MyTableLinks.text + ',' + tableHeader.text + ',' + '\n').encode('utf-8'))
+
+							print "Grabbing box info brah!!!"
+				else:
+
+					print "No box info brah :( Bummer."
+
+					f2.write('\n' + '\n' + links.text + " has no box info brah. Bummer." + '\n' + '\n')
+	elif canonCheck is None: 
+		infoBox = soup2.find('table', {'class' : 'toccolours'}) or soup2.find('table', {'class' : 'wikitable'})
+
+		if infoBox is not None:
+
+			print "We have a box"
+
+			rows = infoBox.select('tr') 
+
+			for tableHeader in rows:
+
+				tableHeader.select('th')
+
+				f2.write((tableHeader.text + ',').encode('utf-8'))
+
+			for tableLinks in rows:
+
+				MyTableLinks = tableLinks.find('a')
+
+				if MyTableLinks is not None:
+					#if MyTableLinks 
+					f2.write(('\n' + MyTableLinks['href'] + ',' + MyTableLinks.text + ',' + tableHeader.text + ',' + '\n').encode('utf-8'))
+
+					print "Grabbing box info brah!!!"
+		else:
+
+			print "No box info brah :( Bummer."
+
+			f2.write('\n' + '\n' + links.text + " has no box info brah. Bummer." +'\n' + '\n' )
+
+f.close()
+f2.close()
+
+#***************2ND PAGE*************************2ND PAGE*************************2ND PAGE**********
+#***************2ND PAGE*************************2ND PAGE*************************2ND PAGE**********
+#***************2ND PAGE*************************2ND PAGE*************************2ND PAGE**********
+#***************2ND PAGE*************************2ND PAGE*************************2ND PAGE**********
+f = open('enemyNames.txt','a')
+
+r = requests.get('http://zeldawiki.org/index.php?title=Category:Enemies&pagefrom=Hover#mw-pages')
+
+soup = BeautifulSoup(r.text)
+
+nameDiv = soup.find("div", { "id" : "mw-pages" })
+
+nameLi= nameDiv.select('li a')
+
+for links in nameLi:
+
+	f.write((links.text + ',' + links['href'] + '\n').encode('utf-8'))
+	#triple = ''
+	#triple = '<http://zeldawiki.org' + links['href'] + '>'
+# the variable triple is currently not being used.
+#f.close()
+	f2 = open('enemyData.txt', 'a')
+
+	r2 = requests.get('http://zeldawiki.org' + links['href'])
+
+	time.sleep(1)
+
+	print 'requesting:', 'http://zeldawiki.org' + links['href']
+
+	soup2 = BeautifulSoup(r2.text)
+
+	canonCheck = soup2.find('table', {'class' :'gradient'}) 
+	if canonCheck is not None:
+		checkingCanon = canonCheck.select('a')
+		print "Dude, I'm gonna check the canon."
+		if checkingCanon is not None:
+			canonLink = raw_input('Oh schnap son! This Canon for' + ' ' + links.text + ' ' + 'is all suspect brah! Do you want me to toss her?')
+		#True == 'y'
+		#False == 'n'
+			if canonLink == 'y':
+				print "Aight, I'm tossing!"
+				pass
+			elif canonLink == 'n':
+				print "Cool dude, I'm gonna check for a box." 
+				infoBox = soup2.find('table', {'class' : 'toccolours'}) or soup2.find('table', {'class' : 'wikitable'})
+
+				if infoBox is not None:
+
+					print "We have a box!"
+
+					rows = infoBox.select('tr')
+
+					for tableHeader in rows:
+
+						tableHeader.select('th')
+
+						f2.write((tableHeader.text + ',').encode('utf-8'))
+
+					for tableLinks in rows:
+
+						MyTableLinks = tableLinks.find('a')
+
+						if MyTableLinks is not None:
+							#if MyTableLinks 
+							f2.write(('\n' + MyTableLinks['href'] + ',' + MyTableLinks.text + ',' + tableHeader.text + ',' + '\n').encode('utf-8'))
+
+							print "Grabbing box info brah!!!"
+				else:
+
+					print "No box info brah :( Bummer."
+
+					f2.write('\n' + links.text + " has no box info brah. Bummer." +'\n' )
+	elif canonCheck is None: 
+		infoBox = soup2.find('table', {'class' : 'toccolours'}) or soup2.find('table', {'class' : 'wikitable'})
+
+		if infoBox is not None:
+
+			print "We have a box"
+
+			rows = infoBox.select('tr')
+
+			for tableHeader in rows:
+
+				tableHeader.select('th')
+
+				f2.write((tableHeader.text + ',').encode('utf-8'))
+
+			for tableLinks in rows:
+
+				MyTableLinks = tableLinks.find('a')
+
+				if MyTableLinks is not None:
+					#if MyTableLinks 
+					f2.write(('\n' + MyTableLinks['href'] + ',' + MyTableLinks.text + ',' + tableHeader.text + ',' + '\n').encode('utf-8'))
+
+					print "Grabbing box info brah!!!"
+		else:
+
+			print "No box info brah :( Bummer."
+
+			f2.write('\n' + '\n' + links.text + " has no box info brah. Bummer." +'\n' + '\n' )
+
+f.close()
+f2.close()
+#***************3RD PAGE*************************3RD PAGE*************************3RD PAGE**********
+#***************3RD PAGE*************************3RD PAGE*************************3RD PAGE**********
+#***************3RD PAGE*************************3RD PAGE*************************3RD PAGE**********
+#***************3RD PAGE*************************3RD PAGE*************************3RD PAGE**********
+f = open('enemyNames.txt','a')
+
+r = requests.get('http://zeldawiki.org/index.php?title=Category:Enemies&pagefrom=Tinsuit#mw-pages')
+
+soup = BeautifulSoup(r.text)
+
+nameDiv = soup.find("div", { "id" : "mw-pages" })
+
+nameLi= nameDiv.select('li a')
+
+for links in nameLi:
+
+	f.write((links.text + ',' + links['href'] + '\n').encode('utf-8'))
+	#triple = ''
+	#triple = '<http://zeldawiki.org' + links['href'] + '>'
+# the variable triple is currently not being used.
+#f.close()
+	f2 = open('enemyData.txt', 'a')
+
+	r2 = requests.get('http://zeldawiki.org' + links['href'])
+
+	time.sleep(1)
+
+	print 'requesting:', 'http://zeldawiki.org' + links['href']
+
+	soup2 = BeautifulSoup(r2.text)
+
+	canonCheck = soup2.find('table', {'class' :'gradient'}) 
+	if canonCheck is not None:
+		checkingCanon = canonCheck.select('a')
+		print "Dude, I'm gonna check the canon."
+		if checkingCanon is not None:
+			canonLink = raw_input('Oh schnap son! This Canon for' + ' ' + links.text + ' ' + 'is all suspect brah! Do you want me to toss her?')
+		#True == 'y'
+		#False == 'n'
+			if canonLink == 'y':
+				print "Aight, I'm tossing!"
+				pass
+			elif canonLink == 'n':
+				print "Cool dude, I'm gonna check for a box." 
+				infoBox = soup2.find('table', {'class' : 'toccolours'}) or soup2.find('table', {'class' : 'wikitable'})
+
+				if infoBox is not None:
+
+					print "We have a box!"
+
+					rows = infoBox.select('tr')
+
+					for tableHeader in rows:
+
+						tableHeader.select('th')
+
+						f2.write((tableHeader.text + ',').encode('utf-8'))
+
+					for tableLinks in rows:
+
+						MyTableLinks = tableLinks.find('a')
+
+						if MyTableLinks is not None:
+							#if MyTableLinks 
+							f2.write(('\n' + MyTableLinks['href'] + ',' + MyTableLinks.text + ',' + tableHeader.text + ',' + '\n').encode('utf-8'))
+
+							print "Grabbing box info brah!!!"
+				else:
+
+					print "No box info brah :( Bummer."
+
+					f2.write('\n' + links.text + " has no box info brah. Bummer." +'\n' )
+	elif canonCheck is None: 
+		infoBox = soup2.find('table', {'class' : 'toccolours'}) or soup2.find('table', {'class' : 'wikitable'})
+
+		if infoBox is not None:
+
+			print "We have a box"
+
+			rows = infoBox.select('tr')
+
+			for tableHeader in rows:
+
+				tableHeader.select('th')
+
+				f2.write((tableHeader.text + ',').encode('utf-8'))
+
+			for tableLinks in rows:
+
+				MyTableLinks = tableLinks.find('a')
+
+				if MyTableLinks is not None:
+					#if MyTableLinks 
+					f2.write(('\n' + MyTableLinks['href'] + ',' + MyTableLinks.text + ',' + tableHeader.text + ',' + '\n').encode('utf-8'))
+					print "Grabbing box info brah!!!"
+		else:
+
+			print "No box info brah :( Bummer."
+
+			f2.write('\n' + '\n' + links.text + " has no box info brah. Bummer." +'\n' + '\n' )
+
+f.close()
+f2.close()
